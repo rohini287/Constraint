b81e241ce104a3e1787dd24fda8f3c80242362c9
rohini287 <hosurkarrohini@gmail.com>
2020-12-21 20:39:05 +0530

    commit the changes

--- Ground.js
+++ Ground.js
@@ -11,7 +11,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("brown");
+      fill(69, 0, 0);
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
